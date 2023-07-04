ef70b39322209e556eb8adeb8eca90c90d65640a
jeong-gumin <jgm@paytalab.com>
2023-07-04 17:21:06 +0900

    init project

---
+++ src/main/kotlin/com/example/stompwebsocketstudy/StompWebsocketStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.stompwebsocketstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StompWebsocketStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<StompWebsocketStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/stompwebsocketstudy/StompWebsocketStudyApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.stompwebsocketstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StompWebsocketStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
